f0c8ee05d7c4e2f27786fa74d73f9a00a549308c
sterago <stefano.rago@agilosoftware.com>
2016-09-27 13:16:23 +0200

    Added basic code for exercise about random data

---
+++ RandomData/src/RandomSentences.java
@@ -0,0 +1,9 @@
+
+public class RandomSentences {
+    public static void main(String[] args) {
+
+        String [] words = {"surround", "mysterious", "stamp", "sneeze", "narrow", "pail", "receive", "enchanting", "aboard", "goofy", "street", "wail"};
+
+
+    }
+}
