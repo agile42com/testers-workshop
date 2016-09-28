8c7ecc74e4856fc641cc47a0a61064680736381f
Niels Verdonk <nverdo@gmail.com>
2016-09-28 11:32:55 +0200

    Added new modules

--- Reflection/src/Foo.java
+++ Reflection/src/Foo.java
@@ -1,16 +1,5 @@
-package reflect;
 
 public class Foo {
-    String string;
-
-    public Foo(){ }
-
-    public Foo(String str){
-        this.string = str;
-    }
-
-    public void print(){
-        System.out.println(string);
-    }
+   
 }
 
--- Reflection/src/Person.java
+++ Reflection/src/Person.java
@@ -2,16 +2,5 @@
  * Created by niels on 27-09-16.
  */
 public class Person {
-    public String firstName;
-    public static String lastName;
 
-    public Person(String first, String last){
-        this.firstName = first;
-        this.lastName = last;
-    }
-
-    @Override
-    public String toString() {
-        return firstName + " " + lastName;
-    }
 }
